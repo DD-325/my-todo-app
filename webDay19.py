12445ce69b8e28884d5d3a6d0731b7e9d2cba909
DarrenD <darrenduerden@googlemail.com>
2022-12-05 22:11:34 +0800

    wording update

--- webDay19.py
+++ webDay19.py
@@ -18,7 +18,7 @@ def add_todo():
 
 st.title('My Todo App')
 st.subheader("This is my todo app")
-st.write("This is to increase your productivity")
+st.write("This is to increase productivity")
 
 
 
